voku/android_packages_apps_ADWLauncher

rewrote the category thing to be less static and for code cleanup

src/org/adw/launcher/ApplicationsAdapter.java
@@ -16,13 +16,19 @@
 
 package org.adw.launcher;
 
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import org.adw.launcher.catalogue.AppCatalogueFilter;
+
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,12 +36,6 @@ import android.widget.ArrayAdapter;
 import android.widget.Filter;
 import android.widget.TextView;
 
-import java.text.Collator;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-
-import org.adw.launcher.catalogue.AppGrpUtils;
 
 /**
  * GridView adapter to show the list of applications and shortcuts
@@ -47,12 +47,15 @@ public class ApplicationsAdapter extends ArrayAdapter<ApplicationInfo> {
 	private boolean useThemeTextColor = false;
     private Typeface themeFont=null;
 	// TODO: Check if allItems is used somewhere else!
-	public static ArrayList<ApplicationInfo> allItems = new ArrayList<ApplicationInfo>();	
+	public static ArrayList<ApplicationInfo> allItems = new ArrayList<ApplicationInfo>();
 	private CatalogueFilter filter;
     private static final Collator sCollator = Collator.getInstance();
+    private final AppCatalogueFilter mCatalogueFilter;
 
-	public ApplicationsAdapter(Context context, ArrayList<ApplicationInfo> apps) {
+	public ApplicationsAdapter(Context context, ArrayList<ApplicationInfo> apps, AppCatalogueFilter filter) {
 		super(context, 0, apps);
+
+		mCatalogueFilter = filter;
 
 		mInflater = LayoutInflater.from(context);
 		// ADW: Load textcolor and bubble color from theme
@@ -117,7 +120,7 @@ public class ApplicationsAdapter extends ArrayAdapter<ApplicationInfo> {
 	@Override
 	public void add(ApplicationInfo info) {
 		boolean changed = false;
-		//check allItems before added. It is a fix for all of the multi-icon issue, but will 
+		//check allItems before added. It is a fix for all of the multi-icon issue, but will
 		//lose performance. Anyway, we do not expected to have many applications.
 		synchronized (allItems) {
 			/*if (!allItems.contains(info)) {
@@ -153,11 +156,11 @@ public class ApplicationsAdapter extends ArrayAdapter<ApplicationInfo> {
 		if(info!=null)
 			super.add(info);
 	}
-	
+
 	void superClear() {
 		super.clear();
 	}
-	
+
 	@Override
 	public void remove(ApplicationInfo info) {
 		boolean changed=false;
@@ -180,16 +183,16 @@ public class ApplicationsAdapter extends ArrayAdapter<ApplicationInfo> {
 				allItems.remove(found);
 				Collections.sort(allItems,new ApplicationInfoComparator());
 				changed=true;
-			}			
+			}
 		}
 		if(changed)
 			updateDataSet();
 	}
 
 	private boolean appInGroup(String s) {
-		return AppGrpUtils.checkAppInGroup(s);
+		return mCatalogueFilter.checkAppInGroup(s);
 	}
-	
+
 	private String getComponentName(ApplicationInfo info) {
 		if (info == null || info.intent == null)
 			return null;
@@ -209,7 +212,7 @@ public class ApplicationsAdapter extends ArrayAdapter<ApplicationInfo> {
 			for (int i = 0; i < length; i++) {
 				ApplicationInfo info = theItems.get(i);
 				String s = getComponentName(info);
-				
+
 				if (s != null && appInGroup(s)) {
 					theFiltered.add(info);
 				}
@@ -221,7 +224,7 @@ public class ApplicationsAdapter extends ArrayAdapter<ApplicationInfo> {
 	{
         getFilter().filter(null);
 	}
-    
+
 	@Override
 	public Filter getFilter() {
 		if (filter == null)
@@ -268,5 +271,5 @@ public class ApplicationsAdapter extends ArrayAdapter<ApplicationInfo> {
         public final int compare(ApplicationInfo a, ApplicationInfo b) {
             return sCollator.compare(a.title.toString(), b.title.toString());
         }
-    }	
+    }
 }

src/org/adw/launcher/catalogue/AppCatalogueFilter.java
@@ -1,0 +1,45 @@
+package org.adw.launcher.catalogue;
+
+import android.content.SharedPreferences;
+
+public class AppCatalogueFilter {
+
+	private AppCatalogueFilters.Catalogue mCatalogue;
+
+	public AppCatalogueFilter() {
+		this(AppGroupAdapter.APP_GROUP_ALL);
+	}
+
+	public AppCatalogueFilter(int index) {
+		setCurrentGroupIndex(index);
+	}
+
+	public boolean checkAppInGroup(String className) {
+		boolean result = true;
+		if (mCatalogue != null) {
+			final SharedPreferences prefs = mCatalogue.getPreferences();
+			if (prefs != null)
+				result = prefs.getBoolean(className, true);
+		}
+		return result;
+	}
+
+	public boolean isUserGroup() {
+		return mCatalogue != null;
+	}
+
+	public int getCurrentFilterIndex() {
+		if (mCatalogue != null)
+			return mCatalogue.getIndex();
+		else
+			return AppGroupAdapter.APP_GROUP_ALL;
+	}
+
+	public synchronized void setCurrentGroupIndex(int index) {
+		if (index != getCurrentFilterIndex()) {
+			mCatalogue = AppCatalogueFilters.getInstance().getCatalogue(index);
+		}
+	}
+
+
+}

src/org/adw/launcher/catalogue/AppGroupAdapter.java
@@ -16,6 +16,12 @@
 
 package org.adw.launcher.catalogue;
 
+import java.util.ArrayList;
+
+import org.adw.launcher.Launcher;
+import org.adw.launcher.R;
+import org.adw.launcher.Workspace;
+
 import android.content.Context;
 import android.content.res.Resources;
 import android.util.Log;
@@ -26,54 +32,41 @@ import android.widget.BaseAdapter;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-
-import org.adw.launcher.Launcher;
-import org.adw.launcher.R;
-
 /**
  * Adapter showing the types of items that can be added to a {@link Workspace}.
  */
 public class AppGroupAdapter extends BaseAdapter {
 
     private final LayoutInflater mInflater;
-    
+
     private final ArrayList<ListItem> mItems = new ArrayList<ListItem>();
-    
-    public static final int APP_GROUP_0   = 0;
-    public static final int APP_GROUP_1   = 1;
-    public static final int APP_GROUP_2   = 2;
-    public static final int APP_GROUP_3   = 3;
-    public static final int APP_GROUP_4   = 4;
-    public static final int APP_GROUP_5   = 5;
-    public static final int APP_GROUP_ALL = 6;
-    public static final int APP_GROUP_CONFIG = 7;
-    public static final int APP_GROUP_ADD = 8;
+
+    public static final int APP_GROUP_ALL = -1;
+    public static final int APP_GROUP_CONFIG = -2;
+    public static final int APP_GROUP_ADD = -3;
     /**
      * Specific item in our list.
      */
     public class ListItem {
         public final CharSequence text;
         public final int actionTag;
-        
-        public ListItem(Resources res, int textResourceId, /*int imageResourceId,*/ int actionTag) {
+
+        public ListItem(Resources res, int textResourceId, int actionTag) {
             text = res.getString(textResourceId);
             this.actionTag = actionTag;
         }
-        public ListItem(Resources res, String textResource,/* int imageResourceId,*/ int actionTag) {
+        public ListItem(Resources res, String textResource, int actionTag) {
             text = textResource;
             this.actionTag = actionTag;
         }
 
     }
 
-	private void addListItem(Resources res, int i, int appGrp/*, int iconId*/)
+	private void addListItem(Resources res, AppCatalogueFilters.Catalogue catalogue)
 	{
-
-		String grpTitle = AppGrpUtils.getGrpTextFromDB(i);
-		if (null != grpTitle) {
-			mItems.add(new ListItem(res, grpTitle,
-						/*iconId, */appGrp));
+		String grpTitle = catalogue.getTitle();
+		if (grpTitle != null) {
+			mItems.add(new ListItem(res, grpTitle, catalogue.getIndex()));
 		}
 	}
 
@@ -81,18 +74,17 @@ public class AppGroupAdapter extends BaseAdapter {
         super();
 
         mInflater = (LayoutInflater) launcher.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        
+
         // Create default actions
         Resources res = launcher.getResources();
-        
-       mItems.add(new ListItem(res, R.string.AppGroupAdd, APP_GROUP_ADD));
-	
-		mItems.add(new ListItem(res, R.string.AppGroupAll,
-					/*R.drawable.app_grp_all,*/ APP_GROUP_ALL));
-		addListItem(res,0,APP_GROUP_0/*,R.drawable.app_grp_multi_media*/);
-		addListItem(res,1,APP_GROUP_1/*,R.drawable.app_grp_others*/);
-		addListItem(res,2,APP_GROUP_2/*,R.drawable.app_grp_tools*/);
-		addListItem(res,3,APP_GROUP_3/*,R.drawable.app_grp_business_tools*/);
+
+        mItems.add(new ListItem(res, R.string.AppGroupAdd, APP_GROUP_ADD));
+		mItems.add(new ListItem(res, R.string.AppGroupAll, APP_GROUP_ALL));
+
+		for(AppCatalogueFilters.Catalogue itm : AppCatalogueFilters.getInstance().getAllGroups()) {
+			addListItem(res, itm);
+		}
+
     }
 
 	public View getView(int position, View convertView, ViewGroup parent) {
@@ -121,5 +113,5 @@ public class AppGroupAdapter extends BaseAdapter {
     public long getItemId(int position) {
         return position;
     }
-    
+
 }

src/org/adw/launcher/catalogue/AppInfoMList.java
@@ -19,40 +19,56 @@ package org.adw.launcher.catalogue;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.adw.launcher.ApplicationInfo;
+import org.adw.launcher.LauncherModel;
+import org.adw.launcher.R;
+import org.adw.launcher.catalogue.AppCatalogueFilters.Catalogue;
+
 import android.app.ListActivity;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.content.SharedPreferences;
-import android.view.Window;
-import android.content.pm.ActivityInfo;
-import org.adw.launcher.*;
-
-import org.adw.launcher.R;
 
 public class AppInfoMList extends ListActivity implements
 		View.OnCreateContextMenuListener, View.OnClickListener,
 		DialogInterface.OnCancelListener {
 	private static final String TAG = "AppInfoMList";
 	private static final boolean DBG = true;
+	public static final String EXTRA_CATALOGUE_INDEX = "EXTRA_CATALOGUE_INDEX";
 
 	// Custom Adapter used for managing items in the list
 	private ApplicationListAdapter mAppInfoAdapter;
 	// list of task info
 	private ListView mAppInfoList;
-	
+
 	private Button mOkButton;
+	private Catalogue mCatalogue;
+
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Intent intent = getIntent();
+		int GroupIndex = intent.getIntExtra(EXTRA_CATALOGUE_INDEX,
+				AppCatalogueFilters.getInstance().getDrawerFilter().getCurrentFilterIndex());
+		mCatalogue = AppCatalogueFilters.getInstance().getCatalogue(GroupIndex);
+		if (mCatalogue == null) {
+			setResult(RESULT_CANCELED);
+			finish();
+			return;
+		}
+
 		requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
 		setContentView(R.layout.app_group_conf_list);
 		getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE,
@@ -77,7 +93,7 @@ public class AppInfoMList extends ListActivity implements
 				updateAppList(isChecked);
 			}
 		});
-		
+
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 		updateAppList();
 	}
@@ -90,13 +106,13 @@ public class AppInfoMList extends ListActivity implements
 
 	/*
 	 * Method implementing functionality of buttons clicked
-	 * 
+	 *
 	 * @see android.view.View.OnClickListener#onClick(android.view.View)
 	 */
 	public void onClick(View v) {
 		if (v == mOkButton) {
 
-			SharedPreferences curAppGrp = AppGrpUtils.getCurAppGrp();
+			SharedPreferences curAppGrp = mCatalogue.getPreferences();
 			if (curAppGrp == null)
 				return;// should not go here.
 
@@ -147,14 +163,14 @@ public class AppInfoMList extends ListActivity implements
 
 		TextView t = (TextView) findViewById(R.id.left_title_text);
 
-		AppGrpUtils.setTitleView(t);
+		mCatalogue.setTitleView(t);
 
-		SharedPreferences curAppGrp = AppGrpUtils.getCurAppGrp();
+		SharedPreferences curAppGrp = mCatalogue.getPreferences();
 
 		for (int i = 0; i < appInfos.size(); i++) {
 			AppListInfo tempAppListInfo = new AppListInfo();
 			/* get App info */
-			ApplicationInfo tempAppInfo = (ApplicationInfo) appInfos.get(i);
+			ApplicationInfo tempAppInfo = appInfos.get(i);
 
 			tempAppListInfo.className = tempAppInfo.intent.getComponent()
 					.flattenToString();

pfalcon/ADW-Launcher

src/com/android/launcher/ApplicationsAdapter.java
@@ -16,13 +16,19 @@
 
 package com.android.launcher;
 
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import com.android.launcher.catalogue.AppCatalogueFilter;
+
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,12 +36,6 @@ import android.widget.ArrayAdapter;
 import android.widget.Filter;
 import android.widget.TextView;
 
-import java.text.Collator;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-
-import com.android.launcher.catalogue.AppGrpUtils;
 
 /**
  * GridView adapter to show the list of applications and shortcuts
@@ -47,12 +47,15 @@ public class ApplicationsAdapter extends ArrayAdapter<ApplicationInfo> {
 	private boolean useThemeTextColor = false;
     private Typeface themeFont=null;
 	// TODO: Check if allItems is used somewhere else!
-	public static ArrayList<ApplicationInfo> allItems = new ArrayList<ApplicationInfo>();	
+	public static ArrayList<ApplicationInfo> allItems = new ArrayList<ApplicationInfo>();
 	private CatalogueFilter filter;
     private static final Collator sCollator = Collator.getInstance();
+    private final AppCatalogueFilter mCatalogueFilter;
 
-	public ApplicationsAdapter(Context context, ArrayList<ApplicationInfo> apps) {
+	public ApplicationsAdapter(Context context, ArrayList<ApplicationInfo> apps, AppCatalogueFilter filter) {
 		super(context, 0, apps);
+
+		mCatalogueFilter = filter;
 
 		mInflater = LayoutInflater.from(context);
 		// ADW: Load textcolor and bubble color from theme
@@ -117,7 +120,7 @@ public class ApplicationsAdapter extends ArrayAdapter<ApplicationInfo> {
 	@Override
 	public void add(ApplicationInfo info) {
 		boolean changed = false;
-		//check allItems before added. It is a fix for all of the multi-icon issue, but will 
+		//check allItems before added. It is a fix for all of the multi-icon issue, but will
 		//lose performance. Anyway, we do not expected to have many applications.
 		synchronized (allItems) {
 			/*if (!allItems.contains(info)) {
@@ -153,11 +156,11 @@ public class ApplicationsAdapter extends ArrayAdapter<ApplicationInfo> {
 		if(info!=null)
 			super.add(info);
 	}
-	
+
 	void superClear() {
 		super.clear();
 	}
-	
+
 	@Override
 	public void remove(ApplicationInfo info) {
 		boolean changed=false;
@@ -180,16 +183,16 @@ public class ApplicationsAdapter extends ArrayAdapter<ApplicationInfo> {
 				allItems.remove(found);
 				Collections.sort(allItems,new ApplicationInfoComparator());
 				changed=true;
-			}			
+			}
 		}
 		if(changed)
 			updateDataSet();
 	}
 
 	private boolean appInGroup(String s) {
-		return AppGrpUtils.checkAppInGroup(s);
+		return mCatalogueFilter.checkAppInGroup(s);
 	}
-	
+
 	private String getComponentName(ApplicationInfo info) {
 		if (info == null || info.intent == null)
 			return null;
@@ -209,7 +212,7 @@ public class ApplicationsAdapter extends ArrayAdapter<ApplicationInfo> {
 			for (int i = 0; i < length; i++) {
 				ApplicationInfo info = theItems.get(i);
 				String s = getComponentName(info);
-				
+
 				if (s != null && appInGroup(s)) {
 					theFiltered.add(info);
 				}
@@ -221,7 +224,7 @@ public class ApplicationsAdapter extends ArrayAdapter<ApplicationInfo> {
 	{
         getFilter().filter(null);
 	}
-    
+
 	@Override
 	public Filter getFilter() {
 		if (filter == null)
@@ -268,5 +271,5 @@ public class ApplicationsAdapter extends ArrayAdapter<ApplicationInfo> {
         public final int compare(ApplicationInfo a, ApplicationInfo b) {
             return sCollator.compare(a.title.toString(), b.title.toString());
         }
-    }	
+    }
 }

src/com/android/launcher/catalogue/AppCatalogueFilter.java
@@ -1,0 +1,45 @@
+package com.android.launcher.catalogue;
+
+import android.content.SharedPreferences;
+
+public class AppCatalogueFilter {
+
+	private AppCatalogueFilters.Catalogue mCatalogue;
+
+	public AppCatalogueFilter() {
+		this(AppGroupAdapter.APP_GROUP_ALL);
+	}
+
+	public AppCatalogueFilter(int index) {
+		setCurrentGroupIndex(index);
+	}
+
+	public boolean checkAppInGroup(String className) {
+		boolean result = true;
+		if (mCatalogue != null) {
+			final SharedPreferences prefs = mCatalogue.getPreferences();
+			if (prefs != null)
+				result = prefs.getBoolean(className, true);
+		}
+		return result;
+	}
+
+	public boolean isUserGroup() {
+		return mCatalogue != null;
+	}
+
+	public int getCurrentFilterIndex() {
+		if (mCatalogue != null)
+			return mCatalogue.getIndex();
+		else
+			return AppGroupAdapter.APP_GROUP_ALL;
+	}
+
+	public synchronized void setCurrentGroupIndex(int index) {
+		if (index != getCurrentFilterIndex()) {
+			mCatalogue = AppCatalogueFilters.getInstance().getCatalogue(index);
+		}
+	}
+
+
+}

src/com/android/launcher/catalogue/AppGroupAdapter.java
@@ -16,6 +16,12 @@
 
 package com.android.launcher.catalogue;
 
+import java.util.ArrayList;
+
+import com.android.launcher.Launcher;
+import com.android.launcher.R;
+import com.android.launcher.Workspace;
+
 import android.content.Context;
 import android.content.res.Resources;
 import android.util.Log;
@@ -26,54 +32,41 @@ import android.widget.BaseAdapter;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-
-import com.android.launcher.Launcher;
-import com.android.launcher.R;
-
 /**
  * Adapter showing the types of items that can be added to a {@link Workspace}.
  */
 public class AppGroupAdapter extends BaseAdapter {
 
     private final LayoutInflater mInflater;
-    
+
     private final ArrayList<ListItem> mItems = new ArrayList<ListItem>();
-    
-    public static final int APP_GROUP_0   = 0;
-    public static final int APP_GROUP_1   = 1;
-    public static final int APP_GROUP_2   = 2;
-    public static final int APP_GROUP_3   = 3;
-    public static final int APP_GROUP_4   = 4;
-    public static final int APP_GROUP_5   = 5;
-    public static final int APP_GROUP_ALL = 6;
-    public static final int APP_GROUP_CONFIG = 7;
-    public static final int APP_GROUP_ADD = 8;
+
+    public static final int APP_GROUP_ALL = -1;
+    public static final int APP_GROUP_CONFIG = -2;
+    public static final int APP_GROUP_ADD = -3;
     /**
      * Specific item in our list.
      */
     public class ListItem {
         public final CharSequence text;
         public final int actionTag;
-        
-        public ListItem(Resources res, int textResourceId, /*int imageResourceId,*/ int actionTag) {
+
+        public ListItem(Resources res, int textResourceId, int actionTag) {
             text = res.getString(textResourceId);
             this.actionTag = actionTag;
         }
-        public ListItem(Resources res, String textResource,/* int imageResourceId,*/ int actionTag) {
+        public ListItem(Resources res, String textResource, int actionTag) {
             text = textResource;
             this.actionTag = actionTag;
         }
 
     }
 
-	private void addListItem(Resources res, int i, int appGrp/*, int iconId*/)
+	private void addListItem(Resources res, AppCatalogueFilters.Catalogue catalogue)
 	{
-
-		String grpTitle = AppGrpUtils.getGrpTextFromDB(i);
-		if (null != grpTitle) {
-			mItems.add(new ListItem(res, grpTitle,
-						/*iconId, */appGrp));
+		String grpTitle = catalogue.getTitle();
+		if (grpTitle != null) {
+			mItems.add(new ListItem(res, grpTitle, catalogue.getIndex()));
 		}
 	}
 
@@ -81,18 +74,17 @@ public class AppGroupAdapter extends BaseAdapter {
         super();
 
         mInflater = (LayoutInflater) launcher.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        
+
         // Create default actions
         Resources res = launcher.getResources();
-        
-       mItems.add(new ListItem(res, R.string.AppGroupAdd, APP_GROUP_ADD));
-	
-		mItems.add(new ListItem(res, R.string.AppGroupAll,
-					/*R.drawable.app_grp_all,*/ APP_GROUP_ALL));
-		addListItem(res,0,APP_GROUP_0/*,R.drawable.app_grp_multi_media*/);
-		addListItem(res,1,APP_GROUP_1/*,R.drawable.app_grp_others*/);
-		addListItem(res,2,APP_GROUP_2/*,R.drawable.app_grp_tools*/);
-		addListItem(res,3,APP_GROUP_3/*,R.drawable.app_grp_business_tools*/);
+
+        mItems.add(new ListItem(res, R.string.AppGroupAdd, APP_GROUP_ADD));
+		mItems.add(new ListItem(res, R.string.AppGroupAll, APP_GROUP_ALL));
+
+		for(AppCatalogueFilters.Catalogue itm : AppCatalogueFilters.getInstance().getAllGroups()) {
+			addListItem(res, itm);
+		}
+
     }
 
 	public View getView(int position, View convertView, ViewGroup parent) {
@@ -121,5 +113,5 @@ public class AppGroupAdapter extends BaseAdapter {
     public long getItemId(int position) {
         return position;
     }
-    
+
 }

src/com/android/launcher/catalogue/AppInfoMList.java
@@ -19,40 +19,56 @@ package com.android.launcher.catalogue;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.android.launcher.ApplicationInfo;
+import com.android.launcher.LauncherModel;
+import com.android.launcher.R;
+import com.android.launcher.catalogue.AppCatalogueFilters.Catalogue;
+
 import android.app.ListActivity;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.content.SharedPreferences;
-import android.view.Window;
-import android.content.pm.ActivityInfo;
-import com.android.launcher.*;
-
-import com.android.launcher.R;
 
 public class AppInfoMList extends ListActivity implements
 		View.OnCreateContextMenuListener, View.OnClickListener,
 		DialogInterface.OnCancelListener {
 	private static final String TAG = "AppInfoMList";
 	private static final boolean DBG = true;
+	public static final String EXTRA_CATALOGUE_INDEX = "EXTRA_CATALOGUE_INDEX";
 
 	// Custom Adapter used for managing items in the list
 	private ApplicationListAdapter mAppInfoAdapter;
 	// list of task info
 	private ListView mAppInfoList;
-	
+
 	private Button mOkButton;
+	private Catalogue mCatalogue;
+
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Intent intent = getIntent();
+		int GroupIndex = intent.getIntExtra(EXTRA_CATALOGUE_INDEX,
+				AppCatalogueFilters.getInstance().getDrawerFilter().getCurrentFilterIndex());
+		mCatalogue = AppCatalogueFilters.getInstance().getCatalogue(GroupIndex);
+		if (mCatalogue == null) {
+			setResult(RESULT_CANCELED);
+			finish();
+			return;
+		}
+
 		requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
 		setContentView(R.layout.app_group_conf_list);
 		getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE,
@@ -77,7 +93,7 @@ public class AppInfoMList extends ListActivity implements
 				updateAppList(isChecked);
 			}
 		});
-		
+
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 		updateAppList();
 	}
@@ -90,13 +106,13 @@ public class AppInfoMList extends ListActivity implements
 
 	/*
 	 * Method implementing functionality of buttons clicked
-	 * 
+	 *
 	 * @see android.view.View.OnClickListener#onClick(android.view.View)
 	 */
 	public void onClick(View v) {
 		if (v == mOkButton) {
 
-			SharedPreferences curAppGrp = AppGrpUtils.getCurAppGrp();
+			SharedPreferences curAppGrp = mCatalogue.getPreferences();
 			if (curAppGrp == null)
 				return;// should not go here.
 
@@ -147,14 +163,14 @@ public class AppInfoMList extends ListActivity implements
 
 		TextView t = (TextView) findViewById(R.id.left_title_text);
 
-		AppGrpUtils.setTitleView(t);
+		mCatalogue.setTitleView(t);
 
-		SharedPreferences curAppGrp = AppGrpUtils.getCurAppGrp();
+		SharedPreferences curAppGrp = mCatalogue.getPreferences();
 
 		for (int i = 0; i < appInfos.size(); i++) {
 			AppListInfo tempAppListInfo = new AppListInfo();
 			/* get App info */
-			ApplicationInfo tempAppInfo = (ApplicationInfo) appInfos.get(i);
+			ApplicationInfo tempAppInfo = appInfos.get(i);
 
 			tempAppListInfo.className = tempAppInfo.intent.getComponent()
 					.flattenToString();