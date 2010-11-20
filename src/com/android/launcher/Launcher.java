052b51c11bc76b0128a9ba9bc92a142ac502ed37
AnderWeb <anderweb@gmail.com>
2010-11-20 13:53:34 +0100

    This is a combination of 20 fixes
    Fix DeleteZone to actually allow uninstalling widgets...
    
    Don't try to unregister scrollable provider if it's not registered
    
    Allow the counter bubbles to show undefined value
    
    Fix the dockbar height, it was too big
    
    fixed several FCs with old homescreen preview
    
    fixed open cursor while editing shirtcuts
    
    fixed 'wrong icon' for notification launcher action
    
    Fix deletezone startDrag FC cause of uninstalled/failed widgets
    
    Fix opening dockbar while disabled
    
    updated german translation
    
    New LauncherActions icons, courtesy of @davekover
    
    Updated LauncherActions to use the new icons
    
    Updated Russian translation for new variables and version number.
    
    Italian translations: Fixed some errors
    
    Fix #45
    
    ADWLauncher: update french translation
    
    Change-Id: I521a90cd96e194551400af33cd8540a4ffde11a9
    
    Updated French translations
    
    - Various new strings
    - Some fixes
    
    Change-Id: I27bb0b64245462473ece70cca41a7f2a4e153e1d
    
    Fix another old bug...
    
    removed unused strings
    removed unused style
    
    removed unused drawables

--- src/com/android/launcher/Launcher.java
+++ src/com/android/launcher/Launcher.java
@@ -349,7 +349,7 @@ public final class Launcher extends Activity implements View.OnClickListener, On
     protected static final int DOCK_STYLE_1=3;
     private int mDockStyle=DOCK_STYLE_3;
     //DRAWER STYLES
-    private int[]mDrawerStyles={R.layout.old_drawer, R.layout.new_drawer};
+    private final int[]mDrawerStyles={R.layout.old_drawer, R.layout.new_drawer};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 		mMessWithPersistence=AlmostNexusSettingsHelper.getSystemPersistent(this);
@@ -1322,7 +1322,7 @@ public final class Launcher extends Activity implements View.OnClickListener, On
         unregisterReceiver(mApplicationsReceiver);
         unregisterReceiver(mCloseSystemDialogsReceiver);
         if(mCounterReceiver!=null)unregisterReceiver(mCounterReceiver);
-        mWorkspace.unregisterProvider();
+        if(scrollableSupport)mWorkspace.unregisterProvider();
     }
 
     @Override
@@ -2252,7 +2252,7 @@ public final class Launcher extends Activity implements View.OnClickListener, On
             return;
         }
         //TODO:ADW Check whether to display a toast if clicked mLAB or mRAB withount binding
-        if(tag instanceof ItemInfo && tag==null && v instanceof ActionButton){
+        if(tag==null && v instanceof ActionButton){
     		Toast t=Toast.makeText(this, R.string.toast_no_application_def, Toast.LENGTH_SHORT);
     		t.show();
     		return;
@@ -3132,7 +3132,6 @@ public final class Launcher extends Activity implements View.OnClickListener, On
 		if(!showDockBar){
 			mDockBar.close();
 		}
-		//mHandleView.setSlidingEnabled(showDockBar);
     	fullScreen(hideStatusBar);
     	if(!mDockBar.isOpen() && !showingPreviews){
 	    	if(!isAllAppsVisible()){
@@ -3398,7 +3397,7 @@ public final class Launcher extends Activity implements View.OnClickListener, On
     	}
     }
     private void dismissPreview(final View v) {
-    	final PopupWindow window = (PopupWindow) v.getTag();
+    	final PopupWindow window = (PopupWindow) v.getTag(R.id.TAG_PREVIEW);
         if (window != null) {
             hideDesktop(false);
             window.setOnDismissListener(new PopupWindow.OnDismissListener() {
@@ -3422,7 +3421,7 @@ public final class Launcher extends Activity implements View.OnClickListener, On
             mWorkspace.invalidate();
             mDesktopLocked=false;
         }
-        v.setTag(null);
+        v.setTag(R.id.TAG_PREVIEW, null);
     }
 
     private void showPreviousPreview(View anchor) {
@@ -3453,7 +3452,7 @@ public final class Launcher extends Activity implements View.OnClickListener, On
 	    	mWorkspace.openSense(true);
     	}else{
 	        //check first if it's already open
-	        final PopupWindow window = (PopupWindow) anchor.getTag();
+	        final PopupWindow window = (PopupWindow) anchor.getTag(R.id.TAG_PREVIEW);
 	        if (window != null) return;
 	    	Resources resources = getResources();
 
@@ -3531,7 +3530,7 @@ public final class Launcher extends Activity implements View.OnClickListener, On
 		                dismissPreview(anchor);
 		            }
 		        });
-		        anchor.setTag(p);
+		        anchor.setTag(R.id.TAG_PREVIEW, p);
 		        anchor.setTag(R.id.workspace, preview);
 		        anchor.setTag(R.id.icon, bitmaps);
 	        }
@@ -4502,6 +4501,6 @@ public final class Launcher extends Activity implements View.OnClickListener, On
     @Override
     public void onSwipe() {
         //TODO: specify different action for each ActionButton?
-        mDockBar.open();
+        if(showDockBar)mDockBar.open();
     }
 }
